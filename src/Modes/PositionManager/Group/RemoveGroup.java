a9a58cf9479697c233b6bdf995ab4b2cc1b1f8ce
AIR <3545210791@qq.com>
2023-08-30 14:42:54 +0800

    update

--- src/Modes/PositionManager/Group/RemoveGroup.java
+++ src/Modes/PositionManager/Group/RemoveGroup.java
@@ -7,7 +7,6 @@ import javafx.scene.layout.VBox;
 
 import java.io.File;
 import java.util.Optional;
-import java.util.Scanner;
 
 /**
  * 本Class用于删除坐标组
@@ -21,29 +20,13 @@ public class RemoveGroup {
      * @param title_value  坐标组的对应的index
      */
     public void entrance(VBox box, String delete_path, String title_value) {
-        // 获取title_value中的组数
-        Scanner sc = new Scanner(title_value);
-        sc.useDelimiter("\\D+");
-        int pos = sc.nextInt() - 1;   // index要比组数小1
-
-        start(box, new File(delete_path), pos);
-    }
-
-    /**
-     * DeleteGroup的真正入口，供方法entrance调用。
-     *
-     * @param box          坐标组显示的VBox
-     * @param delete_file  要删除的坐标组文件
-     * @param pos          坐标组的位置
-     */
-    private void start(VBox box, File delete_file, int pos) {
         Optional<ButtonType> type = WinTool.createAlert(Alert.AlertType.CONFIRMATION,
                 "删除项目", "您是否要删除这一个坐标组", "删除后坐标组中的数据将不复存在");
 
         if (type.get() == ButtonType.OK) {
             // 删除文件
             try {
-                if (!delete_file.delete()) {
+                if (!new File(delete_path).delete()) {
                     throw new RuntimeException();
                 }
             } catch (Exception e) {
