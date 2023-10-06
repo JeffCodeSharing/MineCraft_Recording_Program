f5e1d30948430f7a0b3beb1228431895d30193dc
AIR <3545210791@qq.com>
2023-10-06 18:44:20 +0800

    update

---
+++ src/Tools/JsonTool.java
@@ -0,0 +1,12 @@
+package Tools;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class JsonTool {
+    public static JSONObject read_json() {
+    }
+
+    public static void write_json() {
+
+    }
+}
